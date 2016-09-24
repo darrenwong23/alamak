8ade7fd69991e2e18848851ac7cca51f94a1177e
Admin <admin@Admins-MacBook-Pro.local>
2016-09-24 04:04:28 -0700

    json data

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var app = express();
+var shopdata = require('shopdata.js');
+
 
 app.get('/', function (req, res) {
   res.send('Hello Singapore!');
 });
+
+app.post('/shopdata', function (req, res){
+	res.send(shopdata);
+}
 
 app.listen(3000, function () {
 });
---
+++ server/shopdata.js
@@ -0,0 +1,39 @@
+const shops = [{
+	background: '#2980B9',
+	imgSrc: 'http://www.changiairport.com/content/cag/en/shop-and-dine/shopping/bee-cheng-hiang.img.png',
+	imgBorderColor: '#015389',
+	name: 'Bee Cheng Hiang',
+	title: 'Chocolates, candies & delicatessen',
+	mobileNo: '65423148',
+	location: 'Level 2, Departure Lounge (Central)',
+	description: 'Bee Cheng Hiang’s finger-licking traditional meat snacks such as pork, chicken and beef bakkwa are a hit among Singaporeans who has been enjoying them for decades.'
+}, {
+	background: '#27AE60',
+	imgSrc: 'http://www.changiairport.com/content/cag/en/shop-and-dine/shopping/burberry.img.png',
+	imgBorderColor: '#086C32',
+	name: 'Burberry',
+	title: 'Fashion & Accessories',
+	mobileNo: '0491 570 157',
+	location: 'Level 2, Departure Lounge (Near F Gates)',
+	role: "Drop by to check out the latest women’s arrivals, including a collection of women’s bags, from totes and bowling bags to clutches and cross-bodies, all available in a variety of leathers and Canvas check."
+}, {
+	background: '#9B27AE',
+	imgSrc: 'http://www.changiairport.com/content/cag/en/shop-and-dine/shopping/bonia-natural.img.png',
+	imgBorderColor: '#6A067A',
+	name: 'Bonia Natural',
+	title: 'Fashion & Accessories',
+	mobileNo: '0491 570 158',
+	location: 'Departure Transit Lounge Central, Level 2, Transit Area',
+	role: "With an impressive network of 883 outlets and 103 boutiques worldwide, Bonia is a leader in the Asian leather fashion industry. The brand carries a variety of high-quality leather goods for men and women. "
+}, {
+	background: '#e67e22',
+	imgSrc: 'http://www.changiairport.com/content/cag/en/shop-and-dine/shopping/bottega-veneta.img.png',
+	imgBorderColor: '#9D4F09',
+	name: 'Bottega Veneta',
+	title: 'Fashion & Accessories',
+	mobileNo: '0491 570 110',
+	location: 'MLevel 2, Departure Lounge (Near F Gates)',
+	role: "The company’s famous motto, “When your own initials are enough,” expresses a philosophy of individuality and confidence that now applies to a range of products including women’s and men’s ready-to-wear, fine jewelry, furniture, and more."
+}];
+
+
